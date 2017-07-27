00cd530fb594c63f429242aae3a8f32c7a214f81
James Liu <jamescliu@hotmail.com>
2017-07-27 14:24:22 +0100

    distcp_pairs added

--- terraform/globalvar.tf
+++ terraform/globalvar.tf
@@ -17,7 +17,7 @@ variable "aws_region" {
 
 variable "whitelisted_source" {
    description = "this is the public address of your PC"
-   default = "185.69.145.189/32"
+   default = "185.69.145.214/32"
 }
 
 variable "ami" {
